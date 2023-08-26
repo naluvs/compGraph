9a6cf938d19f50992d3494188b3de206203a2b1f
Ana Lu <86859757+naluvs@users.noreply.github.com>
2023-08-26 19:07:02 -0300

    Create sketch.js

---
+++ solarSystem/sketch.js
@@ -0,0 +1,39 @@
+function setup() {
+  createCanvas(800, 600);
+  rectMode(CENTER)
+}
+
+function draw(){
+  background(color(4, 0, 58), 70);
+  translate(width/2, height/2); 
+  rotate(frameCount * 0.01);
+
+  fill("yellow");
+  circle(0,0,100);
+
+  terra();
+  mercurio();
+}
+
+function terra(){
+  push();
+  translate(width/4, 0);
+  rotate(frameCount * 0.01); 
+  fill('blue');
+  circle(0,0,50);
+
+  translate(width/12, 0);
+  rotate(frameCount * 0.01);
+  fill("grey");
+  circle(0,0,25);
+  pop();
+}
+
+function mercurio(){
+  push();
+  translate(width/10, 0);
+  rotate(frameCount * 0.01);
+  fill('red');
+  circle(0,0,20);
+  pop();
+}
